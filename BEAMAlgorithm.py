be2339a555030f9b61841f054c9c0925a5bc0555
rmgu <rmgu@grasdk.com>
2012-10-01 14:41:39 +0200

    Implement band maths GPF.

--- BEAMAlgorithm.py
+++ BEAMAlgorithm.py
@@ -104,7 +104,7 @@ class BEAMAlgorithm(GeoAlgorithm):
             else:
                 # add a source product
                 if isinstance(param, ParameterRaster) and operator.text != "Read":
-                    # if the source is a file, then add a "sourceProduct" element
+                    # if the source is a file, then add an external "source product" file
                     if os.path.isfile(param.value):
                         source = SubElement(sources, param.name)
                         source.text = "${"+param.name+"}"
@@ -115,7 +115,27 @@ class BEAMAlgorithm(GeoAlgorithm):
                         source.text = param.value   
                 # add parameters
                 else:
-                    parameter = SubElement(parametersNode, param.name)
+                    # Set the name of the parameter
+                    # First check if there are nested tags
+                    tagList = param.name.split(">")
+                    parentElement = parametersNode
+                    parameter = None
+                    for tag in tagList:
+                        # if this is the last tag or there are no nested tags create the parameter element
+                        if tag == tagList[-1]:
+                            parameter = SubElement(parentElement, tag)
+                        # "!" means that a new element in the graph should be created as child of the parent element and set as a parent    
+                        elif tag.startswith("!"):
+                            parentElement = SubElement(parentElement, tag[1:])
+                        # otherwise just find the last element with required name and set it as parent of the parameter element
+                        # or create a new one if it can't be found    
+                        else:
+                            if len(parentElement.findall(tag)) > 0:
+                                parentElement = (parentElement.findall(tag))[-1]
+                            else:
+                                parentElement = SubElement(parentElement, tag)
+                    
+                    # Set the value of the parameter    
                     if isinstance(param, ParameterBoolean):
                         if param.value:
                             parameter.text = "True"
